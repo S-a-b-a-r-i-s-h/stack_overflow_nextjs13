75dbce7aece2945e2d9b0e7e6c1e59b93f8abda6
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2023-09-29 18:16:36 +0530

    navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,45 +1,46 @@
-"use client"
+"use client";
 
-import React,{ createContext, useContext, useState, useEffect } from "react"
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextType {
-    mode: string;
-    setMode: (mode: string) => void;
+  mode: string;
+  setMode: (mode: string) => void;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
-export function ThemeProvider({ children }: {
-children: React.ReactNode }) {
-    const [mode, setMode] = useState('');
-    
-    const handleThemeChange = () => {
-        if(mode === 'dark'){
-            setMode('light');
-            document.documentElement.classList.add('light');
-        } else {
-            setMode('dark');
-            document.documentElement.classList.add('dark');
-        }
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
+
+  const handleThemeChange = () => {
+    if (localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && 
+    window.matchMedia("(prefers-color-scheme:dark)").matches)) {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
+  };
 
-    useEffect(() => {
-        handleThemeChange();
-    },[mode])
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
 
-    return (
-        <ThemeContext.Provider value={{mode,setMode}}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
 export function useTheme() {
-    const context = useContext(ThemeContext);
+  const context = useContext(ThemeContext);
 
-    if(context === undefined) {
-        throw new Error('useTheme must be used within a ThemeProvider')
-    }
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
 
-    return context;
+  return context;
 }
