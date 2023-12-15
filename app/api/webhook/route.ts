ef0671334ebeff87bdf745c55b5e9648e896da92
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2023-12-15 22:12:49 +0530

    changed middleware

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -53,7 +53,7 @@ export async function POST(req: Request) {
  
   // Get the ID and type
   const eventType = evt.type;
- 
+  console.log({eventType});
   if(eventType === 'user.created') {
     const { id,email_addresses,image_url,username,first_name,last_name } = evt.data;
     
