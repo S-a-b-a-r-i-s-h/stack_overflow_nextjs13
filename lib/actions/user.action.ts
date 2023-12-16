5499aca0414660cded3ebd2e15e8475f391a6202
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2023-12-16 22:04:51 +0530

    community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,9 +2,10 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose"
-import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { CreateUserParams, DeleteUserParams, GetAllUsersParams, UpdateUserParams } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+
 
 export async function getUserById(params: any) {
   try {
@@ -80,4 +81,20 @@ export async function deleteUser(params: DeleteUserParams) {
     console.log(error);
     throw error;
   }
+}
+
+export async function getAllUsers(params: GetAllUsersParams) {
+    try {
+        connectToDatabase();
+
+        // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+        const users = await User.find({})
+            .sort({ createdAt: -1 })
+
+        return { users };
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
