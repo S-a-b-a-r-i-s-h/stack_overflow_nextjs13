4c6c92613de50e9f939df4901f3ef738bde610eb
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2023-12-17 15:02:02 +0530

    questiondetails

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -23,6 +23,7 @@ import { Badge } from "../ui/badge";
 import Image from "next/image";
 import { createQuestion } from "@/lib/actions/question.action";
 import { useRouter, usePathname } from 'next/navigation';
+import { useTheme } from "@/context/ThemeProvider";
 
 const type:any = "create"
 
@@ -31,6 +32,7 @@ interface Props {
 }
 
 const Question = ({ mongoUserId }: Props) => {
+  const { mode } = useTheme();
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
@@ -178,6 +180,8 @@ const Question = ({ mongoUserId }: Props) => {
                       "codesample | bold italic forecolor | alignleft aligncenter |" +
                       "alignright alignjustify | bullist numlist ",
                     content_style: "body { font-family:Inter; font-size:16px }",
+                    skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
+                    content_css: mode === 'dark' ? 'dark' : 'light',
                   }}
                 />
               </FormControl>
--- lib/validations.ts
+++ lib/validations.ts
@@ -11,3 +11,7 @@ export const QuestionsSchema = z.object({
   }),
   tags: z.array(z.string().min(1).max(15)).min(1).max(3),
 });
+
+export const AnswerSchema = z.object({
+  answer: z.string().min(100),
+})
