85689671ad68ad8b499d5774cadde4cc2a6dd356
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2024-02-16 18:08:40 +0530

    error correction

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -12,7 +12,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Page = async ({ params, searchParams }) => {
+const Page = async ({ params, searchParams }: any) => {
   const { userId: clerkId } = auth();
 
   let mongoUser;
@@ -85,7 +85,7 @@ const Page = async ({ params, searchParams }) => {
       <ParseHTML data={result.content} />
 
       <div className="mt-8 flex flex-wrap gap-2">
-        {result.tags.map((tag) => (
+        {result.tags.map((tag: any) => (
           <RenderTag
             key={tag._id}
             _id={tag._id}
