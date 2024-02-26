45e7bf3b7be70d2551799c6da6e7f05d715f9b4d
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2024-02-26 15:09:58 +0530

    clerkbug

--- components/shared/QuestionTab.tsx
+++ components/shared/QuestionTab.tsx
@@ -6,7 +6,7 @@ import Pagination from "./Pagination";
 
 interface Props extends SearchParamsProps {
   userId: string;
-  clerkId?: string;
+  clerkId?: string | null;
 }
 
 const QuestionTab = async ({ searchParams, userId, clerkId }: Props) => {
